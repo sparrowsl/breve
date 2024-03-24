658a34cc1e437b7b3bbed750afa0733f1d4d74a4
sparrowsl <benthorpe.dev@gmail.com>
2024-03-24 07:36:22 +0000

    chore: add dynamic api url, add link icon

--- client/src/routes/Card.svelte
+++ client/src/routes/Card.svelte
@@ -1,16 +1,21 @@
 <script>
+  import { PUBLIC_API_URL } from "$env/static/public";
+
   const { link } = $props();
 </script>
 
 <section class="rounded-md border-2 p-4 max-w-md">
   <div>
-    <p>Breve: http://localhost:5000/r/{link?.url}</p>
+    <p>Breve: {PUBLIC_API_URL}/links/r/{link?.url}</p>
     <p>Redirect: {link?.redirect}</p>
     <p>Clicked: {link?.clicked}</p>
   </div>
   <div
     class="flex items-center justify-between mt-5 text-sm text-white font-bold"
   >
+    <a href="{PUBLIC_API_URL}/links/r/{link?.url}" class="block">
+      <i class="gg-link text-black"></i>
+    </a>
     <button class="block bg-teal-600 px-4 py-1 rounded-md">Update</button>
     <button class="block bg-red-500 px-4 py-1 rounded-md">Delete</button>
   </div>
